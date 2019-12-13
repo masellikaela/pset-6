75881986df398abdfc90dd4fdd21fc75696da468
Kaela Maselli <kmaselli@ucvts.org>
2019-12-13 10:45:12 -0500

    wow

---
+++ js/app.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    document.getElementById("addItem").onclick = items;
+}
+
+const items = function() {
+    const drawing = document.getElementById("student-canvas-1").getContext("2d");
+    let item
+
+    if (item !== null){
+       let item = prompt("Add an item to your list: ")
+    }
+  };
