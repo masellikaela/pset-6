2291178c3d1338e80731522e67992108f729ae68
Kaela Maselli <kmaselli@ucvts.org>
2019-12-19 10:44:43 -0500

    why

--- js/app.js
+++ js/app.js
@@ -4,9 +4,17 @@ window.onload = function() {
 
 const items = function() {
     const drawing = document.getElementById("student-canvas-1").getContext("2d");
-    let item
+    let addItem = ("")
 
-    if (item !== null){
-       let item = prompt("Add an item to your list: ")
+    if (addItem !== null){
+        addItem = prompt("Add an item to your list: ")
+        addItem.font = "72px sans-serif";
+        drawing.fillText(addItem, 40, 80, 1024);
+
+
+    } else {
+      drawing.clearRect(0, 0, 1024, 1024);
+
     }
+
   };
