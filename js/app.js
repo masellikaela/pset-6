50b90c6dd7edd5446d13830ef631f48a6b3b87be
Kaela Maselli <kmaselli@ucvts.org>
2020-01-03 10:42:59 -0500

    ok

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
 window.onload = function() {
-    document.getElementById("addItem").onclick = items;
+    document.getElementById("addItem").onclick = additems;
 }
 
-const items = function() {
+const additems = function() {
     const drawing = document.getElementById("student-canvas-1").getContext("2d");
     let addItem = ("")
 
     if (addItem !== null){
-        addItem = prompt("Add an item to your list: ")
         addItem.font = "72px sans-serif";
         drawing.fillText(addItem, 40, 80, 1024);
-
-
     } else {
       drawing.clearRect(0, 0, 1024, 1024);
-
     }
+
 
   };
