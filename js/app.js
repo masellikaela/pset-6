106c80c397f352680b9910d3155b669b34d0fc54
Kaela Maselli <kmaselli@ucvts.org>
2020-01-06 11:54:53 -0500

    im still confused

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
 window.onload = function() {
     document.getElementById("add-button").onclick = additems;
+
 }
 
 const items = [];
+
+const setListeners = function(id) {
+  document.getElementById(id).addEventListener('click', function() {
+    if (id.startWith("priority")) {
+      var mark = document.getElementById("priority");
+      if (mark.class = "highPriority priorityIndicator") {
+        mark.class = "normalPriority priorityIndicator"
+      } else if (mark.class = "normalPriority priorityIndicator") {
+        mark.class = "highPriority priorityIndicator";
+      }
+    } else if (id.startWith("check")) {
+
+    } else if (id.startWith("x")) {
+
+    }
+  })
+}
 
 const additems = function() {
   const text = document.getElementById("textbox").value;
@@ -11,7 +29,8 @@ const additems = function() {
     id: items.length + 1,
     priority: "low",
     content: text,
-    complete: false
+    complete: false,
+    removed: false
   };
   items.push(item);
 
@@ -26,22 +45,54 @@ const renderItems = function() {
 
   for (let i = 0; i < items.length; i++) {
     const li = document.createElement("li");
-    const span1 = document.createElement("span");
-    const span2 = document.createElement("span");
-    const span3 = document.createElement("span");
-    const span4 = document.createElement("span");
+    var priority = document.createElement("span");
+    var listItem = document.createElement("span");
+    var check = document.createElement("span");
+    var x = document.createElement("span");
 
-    span1.innerHTML = "!";
-    span2.innerHTML = items[i].content;
-    span3.innerHTML = "&#10004;";
-    span3.className = items[i].complete === true ? "done" : "not-done";
-    span4.innerHTML = "&#10006;";
-    span4.id = "delete-" + items[i].id;
+    priority.innerHTML = "!";
+    var idNum = 1;
+    var prevId;
+    try {
+      while (true) {
+        prevId = document.getElementById("priority" + String(idNum));
+        idnum++;
+      }
+    } catch (e) {
+      priority.id = "priority" + String(idNum);
+    }
+    priority.class = "normalPriority priorityIndicator";
+    listItem.innerHTML = items[i].content;
+    listItem.id= "item";
+    check.innerHTML = "&#10004;";
+     idNum = 1;
+     prevId;
+    try {
+      while (true) {
+        prevId = document.getElementById("check" + String(idNum));
+        idnum++;
+      }
+    } catch (e) {
+      priority.id = "check" + String(idNum);
+    }
+    check.className = items[i].complete === true ? "done" : "not-done";
+    x.innerHTML = "&#10006;";
+     idNum = 1;
+     prevId;
+    try {
+      while (true) {
+        prevId = document.getElementById("x" + String(idNum));
+        idnum++;
+      }
+    } catch (e) {
+      priority.id = "x" + String(idNum);
+    }
 
-    li.append(span1);
-    li.append(span2);
-    li.append(span3);
-    li.append(span4);
+
+    li.append(priority);
+    li.append(listItem);
+    li.append(check);
+    li.append(x);
     list.append(li);
   }
 };
