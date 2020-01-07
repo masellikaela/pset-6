f25a97ff9019c1859bc41a1483a171ed7db4a7da
Kaela Maselli <ksmaselli@gmail.com>
2020-01-06 20:49:48 -0500

    nope

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 let elements = []
-var add = document.getElementById("add_button");
-var row = document.getElementsByClassName("row");
-let list_item = document.getElementsByClassName("toDo")
-let priority_button = document.getElementsByClassName("priority_button");
-let complete_button = document.getElementsByClassName("complete_button");
-let remove_button = document.getElementsByClassName("remove_button");
-
+var addButton = document.getElementById("add_button");
+var tableRow = document.getElementsByClassName("row");
+let priorityButton = document.getElementsByClassName("priority_button");
+let completeButton = document.getElementsByClassName("complete_button");
+let removeButton = document.getElementsByClassName("remove_button");
+let listItem = document.getElementsByClassName("to_do")
 
 let element_prioritize;
 
@@ -16,19 +15,21 @@ window.onload = function() {
 const create_item = function() {
   let input = document.getElementById("input_item").value;
   if (input === "") {
+
   }
   else {
-      let toDo = {
+      let to_do = {
           task: input,
           priority: false,
           complete: false,
+          html_row: null,
           html_priority_button: null,
           html_text: null,
-          html_row: null,
           html_remove_button: null
       }
-      elements.push(toDo);
-      let index = elements.indexOf(toDo);
+
+      elements.push(to_do);
+      let index = elements.indexOf(to_do);
 
       elements[index].htmlRow = document.createElement("tr");
       elements[index].htmlRow.setAttribute("class", "row");
@@ -38,38 +39,85 @@ const create_item = function() {
       elements[index].htmlPriorityButton.setAttribute("class", "priority_button");
       elements[index].htmlPriorityButton.innerHTML = "!";
 
-      row[index].append(elements[index].htmlPriorityButton);
+      tableRow[index].append(elements[index].htmlPriorityButton);
 
       elements[index].htmlText = document.createElement("td");
       elements[index].htmlText.innerHTML = elements[index].task;
       elements[index].htmlText.setAttribute("class", "to_do");
 
-      row[index].append(elements[index].htmlText);
+      tableRow[index].append(elements[index].htmlText);
 
       elements[index].htmlCompleteButton = document.createElement("td");
       elements[index].htmlCompleteButton.innerHTML = "&#x2713;";
       elements[index].htmlCompleteButton.setAttribute("class", "complete_button");
 
-      row[index].append(elements[index].htmlCompleteButton);
+      tableRow[index].append(elements[index].htmlCompleteButton);
 
       elements[index].htmlRemoveButton = document.createElement("td");
       elements[index].htmlRemoveButton.setAttribute("class", "remove_button");
       elements[index].htmlRemoveButton.innerHTML = "X";
 
-      row[index].append(elements[index].htmlRemoveButton);
+      tableRow[index].append(elements[index].htmlRemoveButton);
     }
     document.getElementById("input_item").value = "";
 };
 
+const remove_item = function() {
+  var removed = false;
+  for (let i = 0; i < removeButton.length; i++) {
+    removeButton[i].onclick = function() {
+        removed = true;
+        let remove_element = tableRow[i];
+        remove_element.remove();
+        elements.splice(i, 1);
+    };
+    if (removed) {
+        break;
+    }
+  }
+}
+
+const finish_item = function() {
+  var finish = false;
+  for (let x = 0; x < completeButton.length; x++) {
+    completeButton[x].onclick = function() {
+       if (elements[x].complete == false) {
+         finish = true;
+         listItem[x].style.setProperty("text-decoration", "line-through");
+         listItem[x].style.backgroundColor = "#baff66";
+         completeButton[x].style.backgroundColor = "#baff66";
+         listItem[x].style.color = "black";
+         completeButton[x].style.color = "black";
+         completeButton[x].style.borderColor = "black";
+         elements[x].complete = true;
+       }
+       else if (elements[x].complete == true) {
+         completeButton[x].style.backgroundColor = "black";
+         completeButton[x].style.color = "#A4FFA4"
+         completeButton[x].style.borderColor = "#A4FFA4"
+         listItem[x].style.setProperty("text-decoration", "none");
+         listItem[x].style.backgroundColor = "black";
+         listItem[x].style.color = "#A4FFA4";
+         elements[x].complete = false;
+       }
+     };
+     if (finish) {
+       break;
+     }
+  }
+}
+
 const prioritize_item = function() {
   var prioritize = false;
-  for (let z = 0; z < priority_button.length; z++) {
-    priority_button[z].onclick = function () {
+  for (let z = 0; z < priorityButton.length; z++) {
+    priorityButton[z].onclick = function() {
       if (elements[z].priority == false) {
-        element_prioritize = row[z]
+        element_prioritize = tableRow[z]
         prioritize = true;
-        priority_button[z].style.backgroundColor = "yellow";
-        row[0].before(element_prioritize);
+        priorityButton[z].style.backgroundColor = "#fff98a";
+        priorityButton[z].style.color = "black";
+        priorityButton[z].style.borderColor = "black";
+        tableRow[0].before(element_prioritize);
         elements[z].priority = true;
 
         const objectToMove = elements[z];
@@ -79,9 +127,11 @@ const prioritize_item = function() {
         prioritize = true;
       }
       else if (elements[z].priority) {
-        element_prioritize = row[z]
-        priority_button[z].style.backgroundColor = "white";
-        row[elements.length - 1].after(element_prioritize);
+        element_prioritize = tableRow[z]
+        priorityButton[z].style.backgroundColor = "black";
+        priorityButton[z].style.color = "#A4FFA4";
+        priorityButton[z].style.borderColor = "#A4FFA4";
+        tableRow[elements.length - 1].after(element_prioritize);
         elements[z].priority = false;
 
         let element_move = elements[z];
@@ -102,48 +152,10 @@ const startup = function() {
   prioritize_item();
 }
 
-add.onclick = create_item
+addButton.onclick = create_item
 
 document.getElementById("input_item").addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     document.getElementById("add_button").click();
   }
 });
-
-const remove_item = function() {
-  var removed = false;
-  for (let i = 0; i < remove_button.length; i++) {
-    remove_button[i].onclick = function() {
-        removed = true;
-        let remove_element = row[i];
-        remove_element.remove();
-        elements.splice(i, 1);
-    };
-    if (removed) {
-        break;
-    }
-  }
-}
-const finish_item = function() {
-  var finish = false;
-  for (let x = 0; x < complete_button.length; x++) {
-    complete_button[x].onclick = function() {
-       if (elements[x].complete == false) {
-         finish = true;
-         list_item[x].style.setProperty("text-decoration", "line-through");
-         list_item[x].style.backgroundColor = "LimeGreen";
-         complete_button[x].style.backgroundColor = "LimeGreen";
-         elements[x].complete = true;
-       }
-       else if (elements[x].complete == true) {
-         complete_button[x].style.backgroundColor = "white";
-         list_item[x].style.setProperty("text-decoration", "none");
-         list_item[x].style.backgroundColor = "white";
-         elements[x].complete = false;
-       }
-     };
-     if (finish) {
-       break;
-     }
-  }
-}
